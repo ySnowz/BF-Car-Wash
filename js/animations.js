20ec79c4a68e23f744bef25fd09db5058bb67374
ySnowz <loserss@outlook.pt>
2023-09-18 08:56:27 -0300

    Add JS animations

---
+++ js/animations.js
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////
+/////////////////// SCROLL ANIMATION ///////////////////////
+///////////////////////////////////////////////////////////
+const buttonNavBar = document.querySelectorAll('.navbar-nav a[href^="#"]')
+
+// adicionando a função do click para cada elemento
+buttonNavBar.forEach((item)=>{
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+// pegando o id do elemento e a distância do topo
+function scrollToIdOnClick(_){
+  _.preventDefault()
+  const element = _.target
+  const id = element.getAttribute('href')
+  const DistanceOffTop = document.querySelector(id).offsetTop
+  window.scroll({
+    top: DistanceOffTop - 60,
+    behavior: "smooth"
+  })
+}
+
+const sectionsReaveal = document.querySelectorAll('section')
+
+sectionsReaveal.forEach((section)=>{
+  ScrollReveal().reveal(section)
+})
